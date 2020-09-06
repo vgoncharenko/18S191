7a3ddd9d9c56cf7b2a174692193b5243debd4837
Shashi Gowda <gowda@mit.edu>
2020-09-06 00:31:40 -0400

    fixes

--- lecture_notebooks/seam-carving.jl
+++ lecture_notebooks/seam-carving.jl
@@ -26,20 +26,19 @@ begin
 			"ImageFiltering"])
 end
 
-# ╔═╡ 18388072-efef-11ea-00d2-f74d34786e24
-using ImageFiltering
+# ╔═╡ 0316b94c-eef6-11ea-19bc-dbc959901bb5
+begin
+	using Images
+	using ImageMagick
+	using Statistics
+	using LinearAlgebra
+	using Plots
+	using PaddedViews
+	using ImageFiltering
+end
 
 # ╔═╡ fe19ad0a-ef04-11ea-1e5f-1bfcbbb51302
 using PlutoUI
-
-# ╔═╡ 0316b94c-eef6-11ea-19bc-dbc959901bb5
-using Images
-using ImageMagick
-using Statistics
-using LinearAlgebra
-using Plots
-using PaddedViews
-using ImageFiltering
 
 # ╔═╡ cb335074-eef7-11ea-24e8-c39a325166a1
 md"""
@@ -50,16 +49,14 @@ md"""
    bits of an image in order to shrink it.
 """
 
-# ╔═╡ 65ec23be-efef-11ea-077c-b7cb0ec91b74
-
-
 # ╔═╡ d2ae6dd2-eef9-11ea-02df-255ec3b46a36
 # img = load(download("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Grant_Wood_-_American_Gothic_-_Google_Art_Project.jpg/480px-Grant_Wood_-_American_Gothic_-_Google_Art_Project.jpg"))
-img = load(download("https://imgur.com/RbrCJLX.png"))
+# img = load(download("https://imgur.com/RbrCJLX.png"))
+img = load(download("https://cdn.shortpixel.ai/spai/w_1086+q_lossy+ret_img+to_webp/https://wisetoast.com/wp-content/uploads/2015/10/The-Persistence-of-Memory-salvador-deli-painting.jpg"))
 #img = load(download("https://web.mit.edu/facilities/photos/construction/Projects/stata/1_large.jpg", tempname()))
 
 # ╔═╡ 0b6010a8-eef6-11ea-3ad6-c1f10e30a413
-# brightness of a color is the sum of the r,g,b values (stored as float32's)
+# arbitrarily choose the brightness of a pixel as mean of rgb
 brightness(c::AbstractRGB) = mean((c.r, c.g, c.b))
 
 # ╔═╡ fc1c43cc-eef6-11ea-0fc4-a90ac4336964
@@ -76,9 +73,61 @@ end
 md"""
 # Edge detection
 
+(Spoiler alert!) Here, we use the Sobel edge detection filter we created in Homework 1.
 
-Here we convolve the brightness with the Sobel filter to compute a measure of "gradient" along the `x` and `y` directions.
+
+```math
+\begin{align}
+
+G_x &= \left(\begin{bmatrix}
+1 \\
+2 \\
+1 \\
+\end{bmatrix} \otimes [1~0~-1]
+\right) * A = \begin{bmatrix}
+1 & 0 & -1 \\
+2 & 0 & -2 \\
+1 & 0 & -1 \\
+\end{bmatrix}*A\\
+G_y &= \left(
+\begin{bmatrix}
+1 \\
+0 \\
+-1 \\
+\end{bmatrix} \otimes [1~2~1]
+\right) * A = \begin{bmatrix}
+1 & 2 & 1 \\
+0 & 0 & 0 \\
+-1 & -2 & -1 \\
+\end{bmatrix}*A
+\end{align}
+```
+Here $A$ is the array corresponding to your image.
+We can think of these as derivatives in the $x$ and $y$ directions.
+
+Then we combine them by finding the magnitude of the **gradient** (in the sense of multivariate calculus) by defining
+
+$$G_\text{total} = \sqrt{G_x^2 + G_y^2}.$$
 """
+
+# ╔═╡ da726954-eff0-11ea-21d4-a7f4ae4a6b09
+Sy, Sx = Kernel.sobel()
+
+# ╔═╡ 172c7612-efee-11ea-077a-5d5c6e2505a4
+function shrink_image(image, ratio=5)
+	(height, width) = size(image)
+	new_height = height ÷ ratio - 1
+	new_width = width ÷ ratio - 1
+	list = [
+		mean(image[
+			ratio * i:ratio * (i + 1),
+			ratio * j:ratio * (j + 1),
+		])
+		for j in 1:new_width
+		for i in 1:new_height
+	]
+	reshape(list, new_height, new_width)
+end
 
 # ╔═╡ 089e1fd4-efed-11ea-12dc-09f9e912d4ab
 function clamp_at_boundary(M, i, j)
@@ -113,6 +162,17 @@ function convolve(M, kernel, M_index_func=clamp_at_boundary)
     return new_image
 end
 
+# ╔═╡ 6f7bd064-eff4-11ea-0260-f71aa7f4f0e5
+function edgeness(img)
+	Sy, Sx = Kernel.sobel()
+	b = brightness.(img)
+
+	∇y = convolve(b, Sy)
+	∇x = convolve(b, Sx)
+
+	sqrt.(∇x.^2 + ∇x.^2)
+end
+
 # ╔═╡ dec62538-efee-11ea-1e03-0b801e61e91c
 function show_colored_kernel(kernel)
 	to_rgb(x) = RGB(max(-x, 0), max(x, 0), 0)
@@ -120,102 +180,41 @@ function show_colored_kernel(kernel)
 end
 
 
-# ╔═╡ 69fbcbb0-eef7-11ea-3adf-576546a2420c
-# the 3x3 stencil for energy
-# take a 3x3 sub-matrix and compute the energy.
-function gradient(b)
-    x_energy = b[1,1] + 2b[2,1] + b[3,1] - b[1,3] - 2b[2,3] - b[3,3]
-    y_energy = b[1,1] + 2b[1,2] + b[1,3] - b[3,1] - 2b[3,2] - b[3,3]
-    [x_energy y_energy]
+# ╔═╡ da39c824-eff0-11ea-375b-1b6c6e186182
+show_colored_kernel.((Sy, Sx))
+
+# ╔═╡ 44192a40-eff2-11ea-0ec7-05cdadb0c29a
+begin
+	∇y = convolve(brightness.(img), Sy)
+	∇x = convolve(brightness.(img), Sx)
+	show_colored_kernel([∇x ∇y])
 end
 
-
-# ╔═╡ d6e46ac4-efa7-11ea-3311-c99448c2ad13
-function energy_stencil(b)
-	norm(gradient(b))
-end
-
-# ╔═╡ fb32e0ea-eef8-11ea-2ad2-0d2f1f48a82f
-function apply_stencil(stencil, X)
-	m, n = size(X)
-	[stencil(X[i-1:i+1, j-1:j+1]) for i = 1:m-2, j=1:n-2]
-end
-
-# ╔═╡ a3e459b0-efa5-11ea-35b4-494093ef34e4
+# ╔═╡ b66662e2-eff3-11ea-08e0-1fd53fd0a0de
 let
-	m, n = (10,10)
-	Gray.(pad([i+j for i=1:m,j=1:n] ./ (m+n)))
+	# zoom in on the whale thing
+	show_colored_kernel([∇x[300:end,  260:end-40] ∇y[300:end, 260:end-40]])
 end
 
-# ╔═╡ a14c98c2-efad-11ea-3543-9f92301bf72e
-pad(img)[0,0]
+# ╔═╡ d6a268c0-eff4-11ea-2c9e-bfef19c7f540
+show_colored_kernel(edgeness(img)) # it's all green because norm makes it positive.
 
-# ╔═╡ cf10d358-efae-11ea-0f5b-198d0d26fbb2
+# ╔═╡ f8283a0e-eff4-11ea-23d3-9f1ced1bafb4
+md"""
 
+## Seam carving idea
 
-# ╔═╡ 495de0a0-efa6-11ea-3665-7d12c97fb3cb
-let
-	m, n = (20,20)
-	grid = [i+j for i=1:m,j=1:n] ./ (m+n)
-	gradients = apply_stencil(gradient, pad(grid))
+The idea of seam carving is to find a path from the top of the image to the bottom of the image where the path minimizes the edgness.
 
-	ycoords = (1:m) .* ones(n)'
-	xcoords = ones(m) .* (1:n)'
-
-	∇x = map(first, gradients)
-	∇y = map(last, gradients)
-	
-	quiver(xcoords, ycoords, quiver=vec.((∇x,∇y) .* 2), ratio=1)
-end
-
-# ╔═╡ d71dbf8e-efab-11ea-1f67-5b975a33fbe1
-#=
-
-	
-	ycoords = (1:m) .* ones(n)'
-	xcoords = ones(m) .* (1:n)'
-	
-	∇x = map(first, gradients)
-	∇y = map(last, gradients)
-	
-	quiver(xcoords, ycoords, quiver=vec.((∇x,∇y) .* 2), ratio=1)
-=#
-
-# ╔═╡ 4d62f328-eef9-11ea-25b1-dba6c2fc6274
-energy(X) = apply_stencil(energy_stencil, X)
-
-# ╔═╡ 9b569f8a-eefe-11ea-3bc3-59bc1c60be30
-e = energy(pad(brightness.(img)))
-
-# ╔═╡ 8f80499a-eef9-11ea-1eb4-7d3083555a70
-Gray.(1 .- e./ maximum(e))
-
-# ╔═╡ 172c7612-efee-11ea-077a-5d5c6e2505a4
-function shrink_image(image, ratio=5)
-	(height, width) = size(image)
-	new_height = height ÷ ratio - 1
-	new_width = width ÷ ratio - 1
-	list = [
-		mean(image[
-			ratio * i:ratio * (i + 1),
-			ratio * j:ratio * (j + 1),
-		])
-		for j in 1:new_width
-		for i in 1:new_height
-	]
-	reshape(list, new_height, new_width)
-end
-
-# ╔═╡ 19e6a72e-efee-11ea-0511-5b5b2eac7f60
-
+In other words, this path **minimizes the number of edges it crosses**
+"""
 
 # ╔═╡ 025e2c94-eefb-11ea-12cb-f56f34886334
 md"""
-# Seam with least energy
 
-Now our task is to find a seam that goes from top most row to the bottom most row. At every step in going down, the path is allowed to go south west, south or south east. We want to find a seam with the minimum possible sum of energies.
+At every step in going down, the path is allowed to go south west, south or south east. We want to find a seam with the minimum possible sum of energies.
 
-We start by writing a `least_energy` function which given a matrix of energies, returns
+We start by writing a `least_edgy` function which given a matrix of energies, returns
 a matrix of minimum possible energy starting from that pixel going up to a pixel in the bottom most row.
 """
 
@@ -227,31 +226,34 @@ a matrix of minimum possible energy starting from that pixel going up to a pixel
 # Basic Comp:   e[x,y] += min( e[x-1,y+1],e[x,y],e[x+1,y])
 #               dirs records which one from (-1==SW,0==S,1==SE)
 
-function least_energy(E)
+function least_edgy(E)
 	least_E = zeros(size(E))
 	dirs = zeros(Int, size(E))
 	least_E[end, :] .= E[end, :] # the minimum energy on the last row is the energy
 	                             # itself
-	padded_least_E = pad(least_E, Inf)
+
 	m, n = size(E)
 	# Go from the last row up, finding the minimum energy
 	for i in m-1:-1:1
 		for j in 1:n
-			e, dir = findmin(padded_least_E[i+1, j-1:j+1])
+			j1, j2 = max(1, j-1), min(j+1, n)
+			e, dir = findmin(least_E[i+1, j1:j2])
 			least_E[i,j] += e
-			dirs[i, j] = (-1, 0, 1)[dir]
+			least_E[i,j] += E[i,j]
+			dirs[i, j] = (-1,0,1)[dir + (j==1)]
 		end
 	end
 	least_E, dirs
 end
 
-# ╔═╡ ce433b6a-efad-11ea-1572-2fcb4fe45c84
-
+# ╔═╡ 8b204a2a-eff6-11ea-25b0-13f230037ee1
+least_e, dirs = least_edgy(edgeness(img))
 
 # ╔═╡ 84d3afe4-eefe-11ea-1e31-bf3b2af4aecd
-least_e, dirs = least_energy(e)
+show_colored_kernel(least_e)
 
 # ╔═╡ b507480a-ef01-11ea-21c4-63d19fac19ab
+# direction the path should take at every pixel.
 getindex.(([" ", "↙", "↓", "↘"],), dirs.+3)
 
 # ╔═╡ 7d8b20a2-ef03-11ea-1c9e-fdf49a397619
@@ -292,7 +294,15 @@ function mark_path(img, path)
 end
 
 # ╔═╡ 552fb92e-ef05-11ea-0a79-dd7a6760089a
-mark_path(img, path)
+[mark_path(img, path) mark_path(show_colored_kernel(least_e), path)]
+
+# ╔═╡ ca4a87e8-eff8-11ea-3d57-01dfa34ff723
+let
+	# least energy path of them all:
+	_, k = findmin(least_e[1, :])
+	path = get_seam_at(dirs, k)
+	[mark_path(img, path) mark_path(show_colored_kernel(least_e), path)]
+end
 
 # ╔═╡ 4f23bc54-ef0f-11ea-06a9-35ca3ece421e
 function rm_path(img, path)
@@ -304,19 +314,13 @@ function rm_path(img, path)
 	img′
 end
 
-# ╔═╡ 0892c568-ef2e-11ea-35af-0d4857ee01fc
-rm_path(img, get_seam_at(dirs, 42))
-
-# ╔═╡ 4de89bf6-ef2e-11ea-36be-eb5b11db1270
-(size(img), size(rm_path(img, get_seam_at(dirs, 42))))
-
 # ╔═╡ b401f398-ef0f-11ea-38fe-012b7bc8a4fa
 function shrink_n(img, n)
 	imgs = []
 
-	e = energy(pad(brightness.(img)))
+	e = edgeness(img)
 	for i=1:n
-		least_E, dirs = least_energy(e)
+		least_E, dirs = least_edgy(e)
 		_, min_j = findmin(@view least_E[1, :])
 		seam = get_seam_at(dirs, min_j)
 		img = rm_path(img, seam)
@@ -344,33 +348,26 @@ md"shrunk by $n:"
 # ╔═╡ Cell order:
 # ╠═e196fa66-eef5-11ea-2afe-c9fcb6c48937
 # ╠═0316b94c-eef6-11ea-19bc-dbc959901bb5
-# ╠═cb335074-eef7-11ea-24e8-c39a325166a1
-# ╠═65ec23be-efef-11ea-077c-b7cb0ec91b74
+# ╟─cb335074-eef7-11ea-24e8-c39a325166a1
 # ╠═d2ae6dd2-eef9-11ea-02df-255ec3b46a36
 # ╠═0b6010a8-eef6-11ea-3ad6-c1f10e30a413
 # ╠═fc1c43cc-eef6-11ea-0fc4-a90ac4336964
 # ╟─2afc2e3c-eef7-11ea-3dd4-797539cbf4da
 # ╟─82c0d0c8-efec-11ea-1bb9-83134ecb877e
+# ╠═da726954-eff0-11ea-21d4-a7f4ae4a6b09
+# ╠═da39c824-eff0-11ea-375b-1b6c6e186182
+# ╠═44192a40-eff2-11ea-0ec7-05cdadb0c29a
+# ╠═b66662e2-eff3-11ea-08e0-1fd53fd0a0de
+# ╠═6f7bd064-eff4-11ea-0260-f71aa7f4f0e5
+# ╠═d6a268c0-eff4-11ea-2c9e-bfef19c7f540
+# ╟─172c7612-efee-11ea-077a-5d5c6e2505a4
 # ╟─089e1fd4-efed-11ea-12dc-09f9e912d4ab
 # ╟─fcf46120-efec-11ea-06b9-45f470899cb2
 # ╟─dec62538-efee-11ea-1e03-0b801e61e91c
-# ╠═18388072-efef-11ea-00d2-f74d34786e24
-# ╠═69fbcbb0-eef7-11ea-3adf-576546a2420c
-# ╠═d6e46ac4-efa7-11ea-3311-c99448c2ad13
-# ╠═fb32e0ea-eef8-11ea-2ad2-0d2f1f48a82f
-# ╠═a3e459b0-efa5-11ea-35b4-494093ef34e4
-# ╠═a14c98c2-efad-11ea-3543-9f92301bf72e
-# ╠═cf10d358-efae-11ea-0f5b-198d0d26fbb2
-# ╠═495de0a0-efa6-11ea-3665-7d12c97fb3cb
-# ╠═d71dbf8e-efab-11ea-1f67-5b975a33fbe1
-# ╠═4d62f328-eef9-11ea-25b1-dba6c2fc6274
-# ╠═9b569f8a-eefe-11ea-3bc3-59bc1c60be30
-# ╠═8f80499a-eef9-11ea-1eb4-7d3083555a70
-# ╟─172c7612-efee-11ea-077a-5d5c6e2505a4
-# ╠═19e6a72e-efee-11ea-0511-5b5b2eac7f60
-# ╟─025e2c94-eefb-11ea-12cb-f56f34886334
+# ╟─f8283a0e-eff4-11ea-23d3-9f1ced1bafb4
+# ╠═025e2c94-eefb-11ea-12cb-f56f34886334
 # ╠═acc1ee8c-eef9-11ea-01ac-9b9e9c4167b3
-# ╠═ce433b6a-efad-11ea-1572-2fcb4fe45c84
+# ╠═8b204a2a-eff6-11ea-25b0-13f230037ee1
 # ╠═84d3afe4-eefe-11ea-1e31-bf3b2af4aecd
 # ╠═b507480a-ef01-11ea-21c4-63d19fac19ab
 # ╟─7d8b20a2-ef03-11ea-1c9e-fdf49a397619
@@ -382,9 +379,8 @@ md"shrunk by $n:"
 # ╠═772a4d68-ef04-11ea-366a-f7ae9e1634f6
 # ╠═14f72976-ef05-11ea-2ad5-9f0914f9cf58
 # ╠═552fb92e-ef05-11ea-0a79-dd7a6760089a
+# ╠═ca4a87e8-eff8-11ea-3d57-01dfa34ff723
 # ╠═4f23bc54-ef0f-11ea-06a9-35ca3ece421e
-# ╠═0892c568-ef2e-11ea-35af-0d4857ee01fc
-# ╠═4de89bf6-ef2e-11ea-36be-eb5b11db1270
 # ╠═b401f398-ef0f-11ea-38fe-012b7bc8a4fa
 # ╠═2eb459d4-ef36-11ea-1f74-b53ffec7a1ed
 # ╠═7038abe4-ef36-11ea-11a5-75e57ab51032
